d7147c09537302031f0322ca3bc66cfa426a4e9b
fendi74960 <serpiente61@hotmail.fr>
2022-01-17 17:07:01 +0100

    virer code duplicate

--- src/main/java/EtatCaseConstructible.java
+++ src/main/java/EtatCaseConstructible.java
@@ -6,23 +6,7 @@ public class EtatCaseConstructible extends EtatCase {
 		super();
 		terrain=self;
 	}
-	
-	@Override
-	public int ActionCase(Joueur j) {
-		double payeTemp;
-		if(j.equals(terrain.getProprio()))
-			return 0;//rien
-		else {
-			payeTemp = valeurLoyer();
-			j.Paye(payeTemp);
-			System.out.println("Vous devez payer " + terrain.getProprio().getNom() + " d'un montant " + payeTemp + "$");
-			System.out.println("Votre nouveau solde est " + j.getSolde());
-			double tempSolde = terrain.getProprio().getSolde();
-			terrain.getProprio().Ajoute(payeTemp);
-			System.out.println("Le solde de " + terrain.getProprio().getNom() + " est pass� de " + tempSolde + " � " + terrain.getProprio().getSolde());
-			return 0;
-		}
-	}
+
 
 	@Override
 	public double valeurLoyer() {
